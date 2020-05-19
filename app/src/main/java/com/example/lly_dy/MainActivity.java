3d249c91c5a31bdb596e7b273f2606e325606a30
dy175785 <49028475+dy175785@users.noreply.github.com>
2020-05-19 14:12:33 +0800

    mvp模式加dregger加许多框架集成的一个基础项目

---
+++ app/src/main/java/com/example/lly_dy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lly_dy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author: dengyong
+ * @date: 2020-5-19 11:28:33
+ * @describe:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
